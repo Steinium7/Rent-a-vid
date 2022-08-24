2943fb0b9929064758b96f82377919cfb6934a84
Steinium7 <51533598+Steinium7@users.noreply.github.com>
2022-08-24 17:56:57 +0000

    secrets config for db

--- startup/db.js
+++ startup/db.js
@@ -2,13 +2,13 @@ const winston = require('winston');
 const config = require('config');
 const mongoose = require('mongoose');
 
+const db = config.get('db');
+
 module.exports = function () {
     if (config.get('Local'))
         mongoose
             .connect('mongodb://localhost/rent-a-vid')
             .then(() => winston.info('Connected to Database.'));
     else
-        mongoose
-            .connect('mongodb://localhost/rent-a-vid')
-            .then(() => winston.info('Connected to Database.'));
+        mongoose.connect(db).then(() => winston.info('Connected to Database.'));
 };
